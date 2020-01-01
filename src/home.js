dfa6b4e719b07ed7bc7cd1592d3bf8238664eaf3
othmane <othman-19@hotmail.fr>
2020-01-01 19:03:20 +0100

    fetch data from API and Dispplay Info

--- src/home.js
+++ src/home.js
@@ -14,11 +14,11 @@ function loadHome() {
   activities.setAttribute('id', 'activities');
   for (let i = 0; i < 5; i += 1) {
     const item = document.createElement('li');
-    item.setAttribute('id', `activity ${i}`);
+    item.setAttribute('id', `activity-${i}`);
     activities.appendChild(item);
   }
   const activityList = activities.children;
-  activityList[0].innerHTML = 'Resevation.';
+  activityList[0].innerHTML = 'obj';
   activityList[1].innerHTML = 'Front end pizza party.';
   activityList[2].innerHTML = 'Back end pizza party.';
   activityList[3].innerHTML = 'Full stack pizza party.';
--- src/index.js
+++ src/index.js
@@ -16,22 +16,37 @@ linksList[2].addEventListener('click', loadContact, false);
 document.body.appendChild(loadFooter());
 
 const fetchWeather = async (name) => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${name}&APPID=d917a8125d1d2b9470fefaa25bd2d8e3`, { mode: 'cors' });
-  const data = response.json();
-  console.log(data);
+  try {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${name}&APPID=d917a8125d1d2b9470fefaa25bd2d8e3`, { mode: 'cors' });
+    const data = await response.json();
+    const weatherStatus = data.weather[0].main;
+    const hum = data.main.humidity;
+    const temperature = data.main.temp;
+    const windSpeed = data.wind.speed;
+    const city = data.name;
+    const dataArr = [city, weatherStatus, temperature, hum, windSpeed];
+    console.log(dataArr);
+    return dataArr;
+  } catch (error) {
+    return ('error');
+  }
 };
-// fetchWeather(cityName);
+
+
+const showData = (fields, info) => {
+  for (let i = 0; i < fields.length; i += 1) {
+    const field = fields[i];
+    field.innerHTML = info[i];
+  }
+};
+
+const dataFields = document.querySelectorAll('li');
 const btn = document.getElementById('submitBtn');
 btn.addEventListener('click', (e) => {
   const cityName = document.getElementById('city_name').value;
-  console.log(cityName);
-  fetchWeather(cityName);
+  fetchWeather(cityName).then((result) => {
+    console.log(result);
+    showData(dataFields, result);
+  });
   e.preventDefault();
 });
-
-// btn.addEventListener('click', (e) => {
-// const name = new FormData(document.getElementById('form'));
-// console.log(name);
-// fetchWeather(cityName);
-//  e.preventDefault();
-// });
