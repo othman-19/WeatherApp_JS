975275fbf3f7502e6bd2a3d7c1100dd9f3f0a62c
othmane <othman-19@hotmail.fr>
2020-01-02 19:08:14 +0100

    Add changing image after sumer submit option, display the weather icon

--- src/index.js
+++ src/index.js
@@ -16,10 +16,14 @@ linksList[2].addEventListener('click', loadContact, false);
 document.body.appendChild(loadFooter());
 
 const showData = (fields, info) => {
-  for (let i = 0; i < fields.length; i += 1) {
+  for (let i = 0; i < 5; i += 1) {
     const field = fields[i];
     field.innerHTML = info[i];
   }
+  const mainField = fields[0];
+  const iconLink = `http://openweathermap.org/img/w/${info[5]}.png`;
+  mainField.insertAdjacentHTML('beforeend', `<img class ="weatherIcon" id = info[5] src = ${iconLink}>`);
+  document.body.style.backgroundImage = `url('./Wallpaper/${info[1]}.jpg')`;
 };
 
 const dataFields = document.querySelectorAll('.infoSpan');
@@ -29,11 +33,12 @@ const fetchWeather = async (name) => {
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&APPID=d917a8125d1d2b9470fefaa25bd2d8e3`, { mode: 'cors' });
     const data = await response.json();
     const weatherStatus = data.weather[0].main;
+    const iconId = data.weather[0].icon;
     const hum = data.main.humidity;
     const temperature = data.main.temp;
     const windSpeed = data.wind.speed;
     const city = data.name;
-    const dataArr = [city, weatherStatus, temperature, hum, windSpeed];
+    const dataArr = [city, weatherStatus, temperature, hum, windSpeed, iconId];
     return dataArr;
   } catch (error) {
     showData(dataFields, 'error');
